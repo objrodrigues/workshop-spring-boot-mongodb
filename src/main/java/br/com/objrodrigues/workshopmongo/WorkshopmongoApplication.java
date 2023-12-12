8a65b18eb4068b654ccc674e3c67cea2740b1862
Diego Rodrigues <objrodrigues@gmail.com>
2023-12-12 14:33:15 -0300

    Entity User e REST funcionando

--- src/main/java/br/com/objrodrigues/workshopmongo/WorkshopmongoApplication.java
+++ src/main/java/br/com/objrodrigues/workshopmongo/WorkshopmongoApplication.java
@@ -1,4 +1,4 @@
-package br.com.objrodrigues;
+package br.com.objrodrigues.workshopmongo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
