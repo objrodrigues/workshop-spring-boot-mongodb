9e04b3bd14b23d10dc748131574c994f12774595
Diego Rodrigues <objrodrigues@gmail.com>
2023-12-13 12:31:25 -0300

    Consulta simples com query methods

--- src/main/java/br/com/objrodrigues/workshopmongo/repositories/PostRepository.java
+++ src/main/java/br/com/objrodrigues/workshopmongo/repositories/PostRepository.java
@@ -1,5 +1,7 @@
 package br.com.objrodrigues.workshopmongo.repositories;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import br.com.objrodrigues.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
-	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
