85f8c3b6793cdff4590c1541bc211caf365e739b
Diego Rodrigues <objrodrigues@gmail.com>
2023-12-12 23:53:24 -0300

    Obtendo o usuario por id

--- src/main/java/br/com/objrodrigues/workshopmongo/services/UserService.java
+++ src/main/java/br/com/objrodrigues/workshopmongo/services/UserService.java
@@ -1,11 +1,13 @@
 package br.com.objrodrigues.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.objrodrigues.workshopmongo.domain.User;
+import br.com.objrodrigues.workshopmongo.exception.ObjectNotFoundException;
 import br.com.objrodrigues.workshopmongo.repositories.UserRepository;
 
 @Service
@@ -17,4 +19,9 @@ public class UserService {
 	public List<User> findAll() {
 		return repo.findAll();
 	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		}
 }
