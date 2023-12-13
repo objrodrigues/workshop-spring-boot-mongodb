f39da70b5c06891465893d1f990890e3f43a5fb4
Diego Rodrigues <objrodrigues@gmail.com>
2023-12-13 06:00:15 -0300

    Obtendo post por id

---
+++ src/main/java/br/com/objrodrigues/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package br.com.objrodrigues.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.objrodrigues.workshopmongo.domain.Post;
+import br.com.objrodrigues.workshopmongo.exception.ObjectNotFoundException;
+import br.com.objrodrigues.workshopmongo.repositories.PostRepository;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
